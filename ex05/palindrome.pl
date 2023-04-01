88907f747f54c6b8f3844e5c512828e69cc4a95a
xredm <mariaredkoles@gmail.com>
2023-04-01 16:37:37 +0200

    haha

--- ex05/palindrome.pl
+++ ex05/palindrome.pl
@@ -1,6 +1,8 @@
 print "Enter a string: ";
 $string = <STDIN>;
 chomp($string);
+$string =~ s/[^a-zA-Z0-9]//g;
+$string = lc($string);
 $reverse = reverse($string);
 if ($string eq $reverse) {
     print "The string is a palindrome!\n";
